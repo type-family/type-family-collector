35ee113451b3a5c21a5f6828f97d9226889f6063
joeyklee <jsphknglee@gmail.com>
2020-01-21 10:42:09 -0500

    comments out posenet

--- sandbox/prototype/sketch.js
+++ sandbox/prototype/sketch.js
@@ -8,10 +8,13 @@ const $mainCanvas = document.querySelector('#mainCanvas');
 let video;
 let canvas;
 let bodyPix;
+let poseNet;
+let poses;
 let flippedVideo;
 
 function preload(){
   bodyPix = ml5.bodyPix();
+  
 }
 
 
@@ -43,13 +46,28 @@ function setup() {
     loop();
   })
 
-  segment()
+  
+
+  // flippedVideo = ml5.flipImage(video);
+  // poseNet = ml5.poseNet(modelReady);
+
+  // poseNet.on('pose', function(results) {
+  //   poses = results;
+  // });
 
 }
 
 function segment(){
   flippedVideo = ml5.flipImage(video);
   bodyPix.segment(flippedVideo, gotResults)
+  // poseNet.singlePose(flippedVideo)
+  
+}
+
+function modelReady(){
+  console.log('model ready')
+  segment();
+  
 }
 
 function gotResults(err, results){
@@ -65,11 +83,12 @@ function gotResults(err, results){
 
   // flippedVideo = ml5.flipImage(video);
   image(results.backgroundMask, 0,0, 640, 480)
+  stroke(255,0,0)
+  fill(255, 0,0)
   pop();
 
   // red square
   push()
-  
   // const padX = (640-400)/2;
   // const padY = (480-400)/2;
   rectMode(CENTER)
@@ -80,6 +99,11 @@ function gotResults(err, results){
   noFill();
   rect(0,0, 400, 400);
   pop()
+
+  if(poses){
+    drawSkeleton();
+    drawKeypoints();
+  }
 
 
   segment();
@@ -132,4 +156,39 @@ async function triggerSave(){
   saveFrames('type_family_test', 'png', 1, 15, (data) => {
     console.log(data)
   });
+}
+
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      // if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      // }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
 }
